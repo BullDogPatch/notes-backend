80a5117a334e51b5d5d8ca8970d51da879a2d546
Craig Clayton <craig__clayton@outlook.com>
2024-03-31 20:05:08 +0100

    push

---
+++ index.js
@@ -0,0 +1,95 @@
+const express = require('express');
+const cors = require('cors');
+const app = express();
+
+let notes = [
+  {
+    id: 1,
+    content: 'HTML is easy',
+    important: true,
+  },
+  {
+    id: 2,
+    content: 'Browser can execute only Javascript',
+    important: false,
+  },
+  {
+    id: 3,
+    content: 'GET and POST are the most importand methods of HTTP protocol',
+    important: true,
+  },
+];
+
+app.use(cors());
+
+const requestLogger = (request, response, next) => {
+  console.log('Method:', request.method);
+  console.log('Path:  ', request.path);
+  console.log('Body:  ', request.body);
+  console.log('---');
+  next();
+};
+app.use(express.json());
+app.use(requestLogger);
+
+const unknownEndpoint = (request, response) => {
+  response.status(404).send({ error: 'unknown endpoint' });
+};
+
+app.get('/', (request, response) => {
+  response.send('<h1>Hello Craig!<h1>');
+});
+
+/* get all notes */
+app.get('/api/notes', (request, response) => {
+  response.json(notes);
+});
+
+/* get note with specific id */
+app.get('/api/notes/:id', (request, response) => {
+  const id = Number(request.params.id);
+  const note = notes.find(note => note.id === id);
+  if (note) {
+    response.json(note);
+  } else {
+    response.status(404).end();
+  }
+});
+
+/* generate an id for adding a note */
+const generated = () => {
+  const maxId = notes.length > 0 ? Math.max(...notes.map(n => n.id)) : 0;
+  return maxId + 1;
+};
+/* add a note */
+app.post('/api/notes', (request, response) => {
+  const body = request.body;
+
+  if (!body.content) {
+    return response.status(400).json({
+      error: 'content missing',
+    });
+  }
+
+  const note = {
+    content: body.content,
+    important: Boolean(body.important) || false,
+    id: generated(),
+  };
+
+  notes = notes.concat(note);
+  response.json(note);
+});
+
+/* delete a note at that specific id */
+app.delete('/api/notes/:id', (request, response) => {
+  const id = Number(request.params.id);
+  notes = notes.filter(note => note.id !== id);
+  response.status(204).end();
+});
+
+app.use(unknownEndpoint);
+
+const PORT = 3001;
+app.listen(PORT);
+console.log(`server running on port ${PORT}`);
