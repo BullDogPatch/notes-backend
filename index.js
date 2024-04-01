5c6c4c39eacbc07ac9a58054390d77ea679bff29
Craig Clayton <craig__clayton@outlook.com>
2024-04-01 17:18:00 +0100

    moved dotenv config to top of file

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
+require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
-require('dotenv').config();
 
 const Note = require('./models/notes');
 const app = express();
